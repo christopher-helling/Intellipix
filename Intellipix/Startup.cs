0786e0801125ff5741a7613644918dadde4208fa
christopher-helling <chrish22302@gmail.com>
2018-12-18 16:43:14 -0500

    Add project files.

---
+++ Intellipix/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Intellipix.Startup))]
+
+namespace Intellipix
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
